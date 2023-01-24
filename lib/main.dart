064c1a75b7c156ca5ec23fc2f8dbedfa2b431dd8
Sadia-Saif <sadiasaif.official@gmail.com>
2023-01-24 05:07:19 +0500

    changes done

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokemon/app/bloc_observer.dart';
 import 'package:pokemon/app/view/app.dart';
-import 'package:pokemon/cubits/pokemon/cubit.dart';
-import 'package:pokemon/utils/static_assets.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'routes/app_routes.dart';
-import 'routes/router.dart';
 
 //TODO:Landscape mode should be used
 
